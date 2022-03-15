4dbc713b8400d459cf018778e7a361c7dbfdc736
ki-ka <100897171+ki-ka@users.noreply.github.com>
2022-03-15 11:12:30 +0900

    Initial commit

---
+++ src/App.jsx
@@ -0,0 +1,65 @@
+import React, { useState } from "react";
+import "./styles.css";
+import { InputTodo } from "./components/InputTodo";
+import { IncompleteTodo } from "./components/IncompleteTodo";
+import { CompleteTodo } from "./components/CompleteTodo";
+
+export const App = () => {
+  const [todoText, setTodoText] = useState("");
+  const [incompleteTodos, setIncompleteTodos] = useState([]);
+  const [completeTodos, setCompleteTodos] = useState([]);
+
+  const onChangeTodoText = (event) => setTodoText(event.target.value);
+
+  const onClickAdd = () => {
+    if (todoText === "") return;
+    const newTodos = [...incompleteTodos, todoText];
+    setIncompleteTodos(newTodos);
+    setTodoText("");
+  };
+
+  const onClickComplete = (index) => {
+    const newIncompleteTodos = [...incompleteTodos];
+    newIncompleteTodos.splice(index, 1);
+
+    const addCompleteTodos = [...completeTodos, incompleteTodos[index]];
+
+    setIncompleteTodos(newIncompleteTodos);
+    setCompleteTodos(addCompleteTodos);
+  };
+
+  const onClickDelete = (index) => {
+    const newTodos = [...incompleteTodos];
+    newTodos.splice(index, 1);
+    setIncompleteTodos(newTodos);
+  };
+
+  const onClickReturn = (index) => {
+    const newTodos = [...completeTodos];
+    newTodos.splice(index, 1);
+
+    const ReturnToIncomleteTodos = [...incompleteTodos, completeTodos[index]];
+
+    setCompleteTodos(newTodos);
+    setIncompleteTodos(ReturnToIncomleteTodos);
+  };
+
+  return (
+    <>
+      <InputTodo
+        todoText={todoText}
+        onChangeTodoText={onChangeTodoText}
+        onClickAdd={onClickAdd}
+      />
+      <IncompleteTodo
+        incompleteTodos={incompleteTodos}
+        onClickComplete={onClickComplete}
+        onClickDelete={onClickDelete}
+      />
+      <CompleteTodo
+        completeTodos={completeTodos}
+        onClickReturn={onClickReturn}
+      />
+    </>
+  );
+};
---
+++ src/components/CompleteTodo.jsx
@@ -0,0 +1,23 @@
+import React from "react";
+
+export const CompleteTodo = (props) => {
+  const { completeTodos, onClickReturn } = props;
+
+  return (
+    <div className="complete-area">
+      <p className="title">完了のTODO</p>
+      <ul>
+        {completeTodos.map((todo, index) => {
+          return (
+            <li>
+              <div key={todo} className="list-low">
+                <p>{todo}</p>
+                <button onClick={() => onClickReturn(index)}>戻す</button>
+              </div>
+            </li>
+          );
+        })}
+      </ul>
+    </div>
+  );
+};
